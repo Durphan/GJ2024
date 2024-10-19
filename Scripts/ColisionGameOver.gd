4ef35c044fef5e3be41adbf4d73b39705d00b8e2
FacundoNicolasTorres <facujaritor@gmail.com>
2024-10-19 14:49:38 -0300

    Script de game over

---
+++ Scripts/ColisionGameOver.gd
@@ -0,0 +1,19 @@
+extends CharacterBody3D
+
+#Este scrip tiene que estar en el scrip del player
+#dentro tiene que tener un Area3D y este Area3D un hijo CollisionShape3D
+#Player
+# |_Area3D
+#   |_ CollisionShape3D  
+func _ready():
+	#Conectar la señal de colision para detectar la colision con la pared
+	$Area3D.body_entered.connect(_on_body_entered)
+	
+func _on_body_entered(body):
+	#verifica si el cuerpo con el que colisiona es una pared
+	if body.is_in_group("Pared"): #La pared con la que choca tiene que estar en un grupo llamado Pared
+		game_over()
+
+func game_over():
+	#get_tree().change_scene("res://") Aca va a la direccion de la escena del game over
+	print("Game Over")
